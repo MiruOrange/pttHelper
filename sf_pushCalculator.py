bdcee7dd81671187498fa1913de9cbfff5cade7f
ken <mafiahsu0.1@gmail.com>
2023-05-31 23:04:41 +0800

    FEAT: push數可能會有''的空白情形

--- sf_pushCalculator.py
+++ sf_pushCalculator.py
@@ -1,23 +1,25 @@
 
 def pushCalculator(push):
-    if push == '爆':
-        push = 100
-    elif pushJudge(push):
-        return dislikeCalculator(push)
+    if push == '':
+        return 0
     else:
-        push = int(push)
-    return push
+        if push == '爆':
+            push = 100
+        elif __pushJudge(push):
+            return __dislikeCalculator(push)
+        else:
+            push = int(push)
+        return push
 
-def dislikeCalculator(push):
-    #符合噓文的規則, 噓達10次, 為X1, 以此類推, 至XX
+
+#符合噓文的規則, 噓達10次, 為X1, 以此類推, 至XX
+def __dislikeCalculator(push):
     if push[1] != 'X':
         return -(int(push[1])*10)
     else:
         return -100
 
-def pushJudge(push):
-    # 判斷是否是X1, X2等格式
+# 判斷是否是X1, X2等格式
+def __pushJudge(push):
     if push[0] == 'X':
         return True
-
-print(pushCalculator('爆'))
