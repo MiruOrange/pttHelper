d24af22e14b6654577f9cb1dd48d2e7bee9f20c3
ken <mafiahsu0.1@gmail.com>
2023-06-03 13:50:50 +0800

    FEAT: 新增arg_db.py 將db相關的參數放至此

---
+++ arg_db.py
@@ -0,0 +1,11 @@
+
+forumDict = {
+    'gossip':'https://www.ptt.cc/bbs/Gossiping/index.html',     #八掛版
+    'job':'https://www.ptt.cc/bbs/job/index.html',              #工作版
+    'techJob':'https://www.ptt.cc/bbs/Tech_Job/index.html',    #科技工作版
+    'softJob':'https://www.ptt.cc/bbs/Soft_Job/index.html',    #軟體工作版
+    'partTime':'https://www.ptt.cc/bbs/Part-time/index.html',  #打工版
+    'actuary':'https://www.ptt.cc/bbs/Actuary/index.html'       #精打細算版
+}
+
+databasePath = '/home/ken/pttCrawler/ptt.db'
