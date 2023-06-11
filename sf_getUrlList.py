4fe6c4dd1505800014f16618eda160b9db9f0ed1
ken <mafiahsu0.1@gmail.com>
2023-06-11 08:16:01 +0800

    FEAT: 新增sf_getUrlList.py的註解

--- sf_getUrlList.py
+++ sf_getUrlList.py
@@ -1,4 +1,9 @@
+'''
+兩個函式的目標是要取出不同來源資料的url
 
+1. getUrlList, 是取出爬蟲後的url, 類型為list
+2. getUrlFromTuppleInList, 是取出資料庫撈出的url, 類型為tupple
+'''
 def getUrlList(articleList):
     urlList = []
     for article in articleList:
